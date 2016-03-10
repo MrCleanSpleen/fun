5a39e7a32a7146a8fecd00c159a0b16738f82939
Alexander Green <aeg0322@gmail.com>
2016-03-09 20:38:16 -0500

    save

--- go/src/waffles/main.go
+++ go/src/waffles/main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"fmt"
+	// c "github.com/skilstak/go/colors"
+	i "github.com/skilstak/go/input"
+)
+
+func main() {
+	waffles := i.Ask("Do you like Waffles? ")
+	if waffles == "yes" {
+		pancakes := i.Ask("Do you like pancakes? ")
+		if pancakes == "yes" {
+			french_toast := i.Ask("Do you like french toast? ")
+			if french_toast == "yes" {
+				fmt.Print("Do do dah do, can't wait to get a mouthful!")
+			} else {
+				fmt.Print("Now you shall DIIEEEEEEEEEEEEE!")
+			}
+
+		} else {
+			fmt.Print("Now you shall DIIEEEEEEEEEEEEE!")
+		}
+
+	} else {
+		fmt.Print("Now you shall DIIEEEEEEEEEEEEE!")
+	}
+}
