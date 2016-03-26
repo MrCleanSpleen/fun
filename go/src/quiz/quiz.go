e6282181f6af9b0e2643ea960a0fb4971df32317
Alexander Green <aeg0322@gmail.com>
2016-03-26 07:42:35 -0400

    save

--- go/src/quiz/quiz.go
+++ go/src/quiz/quiz.go
@@ -5,17 +5,19 @@ import (
 	c "github.com/skilstak/go/colors"
 	i "github.com/skilstak/go/input"
 	"strings"
+	t "time"
 )
 
 func Yes() {
-	fmt.Println(c.M + "Huzzah! You are correct!")
+	fmt.Println(c.CL + c.M + "Huzzah! You are correct!")
+	t.Sleep(2 * t.Second)
 }
 func No() {
-	fmt.Println(c.R + "I am sorry, you are incorrect.")
+	fmt.Println(c.CL + c.R + "I am sorry, you are incorrect.")
+	t.Sleep(2 * t.Second)
 }
 func Q1() {
-	a1, err := i.Prompt(c.M + "What color are the unicorns? ")
-	a1
+	a1 := i.Ask(c.CL + c.M + "What color are the unicorns? ")
 	if strings.Contains(a1, "pink") {
 		Yes()
 	} else {
@@ -23,8 +25,7 @@ func Q1() {
 	}
 }
 func Q2() {
-	a2, err := i.Prompt(c.M + "Where are the dancing? ")
-	a2
+	a2 := i.Ask(c.CL + c.M + "Where are the dancing? ")
 	if strings.Contains(a2, "rainbows") {
 		Yes()
 	} else {
@@ -33,8 +34,7 @@ func Q2() {
 }
 
 func Q3() {
-	a3, err := i.Prompt(c.M + "Please use 1 word to describe the texture of their maaaaaaaaaaaagical fur. ")
-	a3
+	a3 := i.Ask(c.CL + c.M + "Please use 1 word to describe the texture of their maaaaaaaaaaaagical fur. ")
 	if strings.Contains(a3, "smiles") {
 		Yes()
 	} else {
