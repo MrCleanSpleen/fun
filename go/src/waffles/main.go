a6681fa754e31d254d3b7a3f6561fb8597a26208
Alexander Green <aeg0322@gmail.com>
2016-03-27 16:10:32 -0400

    save

--- go/src/waffles/main.go
+++ go/src/waffles/main.go
@@ -2,27 +2,27 @@ package main
 
 import (
 	"fmt"
-	// c "github.com/skilstak/go/colors"
 	i "github.com/skilstak/go/input"
 )
 
 func main() {
-	waffles := i.Ask("Do you like Waffles? ")
+	waffles := i.Ask("Do you like waffles? >>> ")
 	if waffles == "yes" {
-		pancakes := i.Ask("Do you like pancakes? ")
+		fmt.Println(":)")
+		pancakes := i.Ask("Do you like pancakes? >>> ")
 		if pancakes == "yes" {
-			french_toast := i.Ask("Do you like french toast? ")
-			if french_toast == "yes" {
-				fmt.Print("Do do dah do, can't wait to get a mouthful!")
+			fmt.Println(":)")
+			toast := i.Ask("Do you like french toast? >>> ")
+			if toast == "yes" {
+				fmt.Println("Do do dah do, can't wait to get a mouthful!")
+
 			} else {
-				fmt.Print("Now you shall DIIEEEEEEEEEEEEE!")
+				fmt.Println("")
 			}
-
 		} else {
-			fmt.Print("Now you shall DIIEEEEEEEEEEEEE!")
+			fmt.Println("")
 		}
-
 	} else {
-		fmt.Print("Now you shall DIIEEEEEEEEEEEEE!")
+		fmt.Println("")
 	}
 }
