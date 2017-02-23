ecb6026f48fc2bed4a521d3b6cfb967d146ed992
Alexander Green <aeg0322@gmail.com>
2017-02-23 17:48:14 -0500

    save

--- go/src/weather/main.go
+++ go/src/weather/main.go
@@ -1,25 +1,44 @@
 package main
+
 import (
+	"encoding/json"
 	"fmt"
-	"net/url"
-	s "strings"
 	"log"
 	"net/http"
-	"encoding/json"
+	"net/url"
+	//	s "strings"
 )
-func weather(loc []string) {
+
+func Weather(loc string) {
 	units := "imperial"
-	m := "main"
-	s := "sys"
-	loct :=	s.ToLower(loc)
-	url := "http://api.openweathermap.org/data/2.5/weather?q=" + loct + "&appid=6a4cbda1e239084151dea640a95d2a0c" + "&units=" + units
+	//	m := "main"
+	//	s := "sys"
+	loct := url.QueryEscape(loc)
+	url := fmt.Sprintf("http://api.openweathermap.org/data/2.5/weather?q=", loct, "&appid=6a4cbda1e239084151dea640a95d2a0c&units=", units)
 	type Data struct {
-		Place	string
-		Temp	string
-		Country	string
-		Temp_min	string
-	}	
+		Name     string `json:"name"`
+		Temp     int    `json:"main,temp"`
+		Country  string `json:"sys,country"`
+		Temp_min int    `json:"main,temp_min"`
+	}
 	req, err := http.NewRequest("GET", url, nil)
-		if err != nil {
-			log.Fatal("NewRequest: ", err)
-			return	
+	if err != nil {
+		log.Fatal("NewRequest: ", err)
+		return
+	}
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Do: ", err)
+		return
+	}
+	var record Data
+	if err := json.NewDecoder(resp.Body).Decode(&record); err != nil {
+		log.Println(err)
+	}
+	fmt.Println("City: ", record.Name)
+	fmt.Println("Temperature: ", record.Temp)
+}
+func main() {
+	Weather("charlotte")
+}
