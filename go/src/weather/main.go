918425344c92c9851e4f8bed8682540ea9b86c40
Alexander Green <aeg0322@gmail.com>
2017-02-28 09:35:04 -0500

    save

--- go/src/weather/main.go
+++ go/src/weather/main.go
@@ -17,12 +17,11 @@ func Weather(loc string) {
 	url := fmt.Sprintf("http://api.openweathermap.org/data/2.5/weather?q=", loct, "&appid=6a4cbda1e239084151dea640a95d2a0c&units=", units)
 	type Data struct {
 		Name string  `json:"name"`
-	//	Temp float64 `json:"main,temp"`
+		Temp float64 `json:"main,temp"`
 		Main	[]struct	{
-			`json:"
 			Temp	float64	`json:"temp"`
 			Pressure	int	`json:"pressure"`
-	}
+		}
 	}
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
@@ -40,8 +39,11 @@ func Weather(loc string) {
 		log.Println(err)
 		//		err := json.Unmarshal(b, &record)
 	}
+	foo2 := {}
+	getJson("http://example.com", &foo2)
+	println(foo2.Bar)
 	fmt.Println("City: ", weather.Name)
-	fmt.Println("Temperature: ", weather.Temp)
+	//	fmt.Println("Temperature: ", weather.Temp)
 	//	fmt.Println("Country: ", weather.Country)
 }
 func main() {
