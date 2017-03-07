a724dfdbac432557190b5cb9618c7f0ddf1ff08d
Alexander Green <aeg0322@gmail.com>
2017-03-07 09:35:03 -0500

    save

--- go/src/weather/main.go
+++ go/src/weather/main.go
@@ -3,48 +3,39 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"log"
+	//"log"
 	"net/http"
 	"net/url"
+	"time"
 	//	s "strings"
 )
 
+func get_json(url string, target interface{}) error {
+	//if it works, it works
+	var myClient = &http.Client{Timeout: 10 * time.Second}
+	r, err := myClient.Get(url)
+	if err != nil {
+		return err
+	}
+	defer r.Body.Close()
+	return json.NewDecoder(r.Body).Decode(target)
+}
 func Weather(loc string) {
 	units := "imperial"
-	//	m := "main"
-	//	s := "sys"
 	loct := url.QueryEscape(loc)
 	url := fmt.Sprintf("http://api.openweathermap.org/data/2.5/weather?q=", loct, "&appid=6a4cbda1e239084151dea640a95d2a0c&units=", units)
 	type Data struct {
 		Name string `json:"name"`
-		//Temp float64 `json:"main,temp"`
-		Main []struct {
-			Temp     float64 `json:"temp"`
-			Pressure int     `json:"pressure"`
-		}
+		Temp int    `json:"main,temp"`
+		//Main []struct {
+		//	Temp     float64 `json:"temp"`
+		//	Pressure int     `json:"pressure"`
+		//}
 	}
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		log.Fatal("NewRequest: ", err)
-		return
-	}
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		log.Fatal("Do: ", err)
-		return
-	}
-	var weather Data
-	if err := json.NewDecoder(resp.Body).Decode(&weather); err != nil {
-		log.Println(err)
-		//		err := json.Unmarshal(b, &record)
-	}
-	//[foo2 := {}
-	//[	getJson("http://example.com", &foo2)
-	//[	println(foo2.Bar)
-	fmt.Println("City: ", weather.Name)
-	//	fmt.Println("Temperature: ", weather.Temp)
-	//	fmt.Println("Country: ", weather.Country)
+	data2 := Data{}
+	get_json(url, &data2)
+	fmt.Println(data2.Temp)
+
 }
 func main() {
 	Weather("charlotte")
