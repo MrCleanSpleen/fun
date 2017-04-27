8a6b65d16f2a2bc527d3c946a80f5be36fdfdf00
Alexander Green <aeg0322@gmail.com>
2017-04-26 20:00:22 -0400

    save

--- go/src/weather/main.go
+++ go/src/weather/main.go
@@ -24,13 +24,19 @@ func Weather(loc string) {
 	units := "imperial"
 	loct := url.QueryEscape(loc)
 	url := fmt.Sprintf("http://api.openweathermap.org/data/2.5/weather?q=", loct, "&appid=6a4cbda1e239084151dea640a95d2a0c&units=", units)
+	type Coord struct {
+		Lon float64 `json:"lon"`
+		Lat float64 `json:"lat"`
+	}
 	type Data struct {
-		Name string `json:"name"`
-		Main []int  `json:"main"`
+		Name string             `json:"name"`
+		Id   int                `json:"id"`
+		Main map[string]float64 `json:"main"`
 	}
 	data2 := Data{}
 	get_json(url, &data2)
-	fmt.Println(data2.Main["temp"])
+	temp := (data2.Main["temp"] * 1.8) - 459.67
+	fmt.Println("In", data2.Name, ", the temperature is", temp, "F", ", with a humidity of", data2.Main["humidity"], "%.")
 }
 func main() {
 	Weather("charlotte")
