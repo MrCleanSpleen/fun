5119996aff24ece9c9d53ccb701ec189964993ae
Alexander Green <aeg0322@gmail.com>
2018-02-09 09:47:34 -0500

    save

--- go/src/weather/main.go
+++ go/src/weather/main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	//"log"
 	"net/http"
-	"net/url"
+	//"net/url"
 	"time"
 	//	s "strings"
 )
@@ -23,8 +23,8 @@ func get_json(url string, target interface{}) error {
 func Weather(loc string) {
 	// Gets weather data for a given city and returns it.
 	units := "imperial"
-	loct := url.QueryEscape(loc)
-	url := fmt.Sprintf("http://api.openweathermap.org/data/2.5/weather?q=", loct, "&appid=b6907d289e10d714a6e88b30761fae22&units=", units)
+	//loct := url.QueryEscape(loc)
+	url := fmt.Sprintf("http://api.openweathermap.org/data/2.5/weather?q=london&appid=b6907d289e10d714a6e88b30761fae22&units=", units)
 	type Coord struct {
 		Lon float64 `json:"lon"`
 		Lat float64 `json:"lat"`
@@ -37,13 +37,12 @@ func Weather(loc string) {
 		Coord   map[string]float64 `json:"coord"`
 		Weather map[string]float64 `json:"weather"`
 	}
+	get_json(url, &Data{})
 	data2 := Data{}
-	get_json(url, &data2)
 	temp := (data2.Main["temp"] * 1.8) - 459.67
 	fmt.Println("In", data2.Name, ", the temperature is", temp, "F", ", with a humidity of", data2.Main["humidity"], "%.")
-	//	fmt.Println(data2.Main[])
+	print(data2.Name)
 }
 func main() {
-	//	Weather("Charlotte")
-	fmt.Println(data2.Main["temp"])
+	Weather("london")
 }
